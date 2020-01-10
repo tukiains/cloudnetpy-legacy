def2da238d0720f647e32111405e268a9ba15e50
Anniina Korpinen <anniina.korpinen@fmi.fi>
2020-01-10 16:38:05 +0200

    Testing classification done

--- tests/unit/test_classification.py
+++ tests/unit/test_classification.py
@@ -1,40 +1,51 @@
 import numpy as np
-from numpy.testing import assert_array_equal, assert_array_almost_equal
 from collections import namedtuple
 import pytest
+from cloudnetpy.products.classification import _get_target_classification, _get_detection_status
 
-CATEGORIZE_BITS = namedtuple('CATEGORIZE_BITS', ['category_bits', 'quality_bits'])
 
-CATEGORIZE_BITS(category_bits={'droplet': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                     [0, 1, 1, 0, 0, 0]], dtype=bool),
-                               'falling': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                      [0, 1, 1, 0, 0, 0]], dtype=bool),
-                               'cold': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                  [0, 1, 1, 0, 0, 0]], dtype=bool),
-                               'melting': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                     [0, 1, 1, 0, 0, 0]], dtype=bool),
-                               'aerosols': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                      [0, 1, 1, 0, 0, 0]], dtype=bool),
-                               'insects': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                     [0, 1, 1, 0, 0, 0]], dtype=bool),
-                               },
-                quality_bits={'radar': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                  [0, 1, 1, 0, 0, 0]], dtype=bool),
-                              'lidar': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                   [0, 1, 1, 0, 0, 0]], dtype=bool),
-                              'clutter': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                    [0, 1, 1, 0, 0, 0]], dtype=bool),
-                              'molecular': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                      [0, 1, 1, 0, 0, 0]], dtype=bool),
-                              'attenuated': np.asarray([[1, 0, 1, 0, 0, 0],
+class CategorizeBits:
+    def __init__(self):
+        self.category_bits = {'droplet': np.asarray([[1, 0, 1, 1, 1, 1],
+                                                     [0, 1, 1, 1, 0, 0]], dtype=bool),
+                              'falling': np.asarray([[0, 0, 0, 0, 1, 0],
+                                                     [0, 0, 0, 1, 1, 1]], dtype=bool),
+                              'cold': np.asarray([[0, 0, 1, 1, 0, 0],
+                                                  [0, 1, 1, 1, 0, 1]], dtype=bool),
+                              'melting': np.asarray([[1, 0, 1, 0, 0, 0],
+                                                     [1, 1, 0, 0, 0, 0]], dtype=bool),
+                              'aerosol': np.asarray([[1, 0, 1, 0, 0, 0],
+                                                      [0, 0, 0, 0, 0, 0]], dtype=bool),
+                              'insect': np.asarray([[1, 1, 0, 0, 0, 0],
+                                                     [0, 0, 1, 0, 0, 0]], dtype=bool),
+                               }
+        self.quality_bits = {'radar': np.asarray([[0, 0, 0, 1, 1, 1],
+                                                  [1, 0, 0, 1, 1, 1]], dtype=bool),
+                             'lidar': np.asarray([[1, 1, 1, 1, 0, 0],
+                                                  [1, 1, 0, 1, 1, 0]], dtype=bool),
+                             'clutter': np.asarray([[0, 0, 1, 1, 0, 0],
+                                                    [0, 0, 0, 0, 0, 0]], dtype=bool),
+                             'molecular': np.asarray([[1, 0, 0, 1, 0, 0],
+                                                      [0, 1, 0, 0, 0, 0]], dtype=bool),
+                             'attenuated': np.asarray([[1, 1, 1, 0, 0, 1],
                                                        [0, 1, 1, 0, 0, 0]], dtype=bool),
-                              'corrected': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                      [0, 1, 1, 0, 0, 0]], dtype=bool)})
+                             'corrected': np.asarray([[1, 0, 0, 0, 0, 0],
+                                                      [1, 1, 0, 0, 0, 0]], dtype=bool)}
 
 
-def test_get_target_classification():
-    assert True
+@pytest.mark.parametrize('value', [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+def test_get_target_classification(value):
+    bits = CategorizeBits()
+    x = _get_target_classification(bits)
+    assert value in x
 
 
-def test_get_detection_status():
-    assert True
+@pytest.mark.parametrize('value', [
+    0, 1, 2, 3, 4, 5, 6, 7])
+def test_get_detection_status_value(value):
+    bits = CategorizeBits()
+    x = _get_detection_status(bits)
+    print("")
+    print(x)
+    assert value in x
